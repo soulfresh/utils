19e54ea9278af4117698dae271fb1a2422edfa63
Marc Wren <things@soul-fresh.com>
2021-05-05 14:18:53 -0600

    Ensure the promises file is exported.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 export * from './colors';
 export * from './dates';
+export * from './promises';
+export * from './services';
 export * from './strings';
 export * from './urls';
-export * from './services';

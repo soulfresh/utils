098b5537b44f3865d187f98d50d46f9b61afdb28
Marc Wren <things@soul-fresh.com>
2022-08-16 12:12:31 -0400

    fix: Export mergeRefs function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 export * from './binary';
 export * from './colors';
 export * from './dates';
+export * from './functions';
 export * from './promises';
 export * from './services';
 export * from './strings';
